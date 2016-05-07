f72a5259902d4622008b8f99c672f1e0343a3bfb
Javier Garmón <jgarmon@inevio.com>
2016-05-07 02:38:44 +0200

    Basic skeleton

---
+++ main.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+'use strict'
+
+// Modules
+var vertigo = require('vertigo');
+
+// Create server
+var server = vertigo.createServer( 21042 );
+
+// Listen petitions
+server.on( 'nodes', require('./cmd/nodes ');
+server.on( 'nodeByName', require('./cmd/nodeByName ');
+server.on( 'nodeByIP', require('./cmd/nodeByIP ');
+server.on( 'addNode', require('./cmd/addNode ');
+server.on( 'addNodeWithName', require('./cmd/addNodeWithName ');
+server.on( 'removeNode', require('./cmd/removeNode ');
+server.on( 'removeNodeByName', require('./cmd/removeNodeByName ');
+server.on( 'addImageToAllNodes', require('./cmd/addImageToAllNodes ');
+server.on( 'removeImageFromAllNodes', require('./cmd/removeImageFromAllNodes ');
+server.on( 'addImageToNodeByIP', require('./cmd/addImageToNodeByIP ');
+server.on( 'addImageToNodeByName', require('./cmd/addImageToNodeByName ');
+server.on( 'removeImageByNodeByIP', require('./cmd/removeImageByNodeByIP ');
+server.on( 'removeImageByNodeByName', require('./cmd/removeImageByNodeByName ');
+server.on( 'updateAllImagesOfAllNodes', require('./cmd/updateAllImagesOfAllNodes ');
+server.on( 'updateImageOfAllNodes', require('./cmd/updateImageOfAllNodes ');
+server.on( 'updateImageOfNode', require('./cmd/updateImageOfNode ');
+server.on( 'scaleImageInAllNode', require('./cmd/scaleImageInAllNode ');
+server.on( 'scaleImageInNodeByIP', require('./cmd/scaleImageInNodeByIP ');
+server.on( 'scaleImageInNodeByName', require('./cmd/scaleImageInNodeByName ');
