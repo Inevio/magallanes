76c141bd355068ccc3e4360737dcbab42fe31aac
Rodrigo Rojo <rrojot@hotmail.com>
2016-05-11 19:15:37 +0200

    Remote addImage function

--- cmd/addImageToNodeByName.js
+++ cmd/addImageToNodeByName.js
@@ -1,3 +1,14 @@
 'use strict';
 
-module.exports = function(){};
+module.exports = function( info, callback ){
+
+    var client = vertigo.createClient({ host: 'magallanes-monitor', port: 21042 });
+
+    client.request('monitorAddImage', 'registry.inevio.com:5000/magallanes-server', function ( err, res ) {
+        console.log(err);
+        console.log(res[0]);
+        console.log(res[1]);
+        console.log(res[2]);
+    });
+
+};
